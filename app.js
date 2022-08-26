a4a1848b4e9fd63cfd06768edc263833ec84be24
Joaquin Rode <Joaquin.Rode@endava.com>
2022-08-26 17:45:33 -0300

    Application deploy - 2022-26-08-17:45

--- app.js
+++ app.js
@@ -12,30 +12,30 @@ hbs.registerPartials(__dirname + '/views/partials');
 //serve static content
 app.use( express.static('public') );
 
-// app.get('/', function (req, res) {
-//   res.render('home', {
-//     name: 'Joaco',
-//     title: 'Nodejs Course'
-//   });
-// });
+app.get('/', function (req, res) {
+  res.render('home', {
+    name: 'Joaco',
+    title: 'Nodejs Course'
+  });
+});
 
-// app.get('/generic', function (req, res) {
-//   res.render('generic', {
-//     name: 'Joaco',
-//     title: 'Nodejs Course'
-//   });
-// });
+app.get('/generic', function (req, res) {
+  res.render('generic', {
+    name: 'Joaco',
+    title: 'Nodejs Course'
+  });
+});
 
-// app.get('/elements', function (req, res) {
-//   res.render('elements', {
-//     name: 'Joaco',
-//     title: 'Nodejs Course'
-//   });
-//   });
+app.get('/elements', function (req, res) {
+  res.render('elements', {
+    name: 'Joaco',
+    title: 'Nodejs Course'
+  });
+  });
 
 
 app.get('*', function (req, res) {
-res.sendFile( __dirname + '/public/index.html');
+res.sendFile( __dirname + '/public/404.html');
 });
 
 
