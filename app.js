633413c54d2e990a9de75c843f09883a07471f1c
Joaquin Rode <Joaquin.Rode@endava.com>
2022-08-26 16:52:09 -0300

    Webserver ready

---
+++ app.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const express = require('express')
+const hbs = require('hbs');
+
+const app = express()
+const port = process.env.PORT;
+
+//Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+//serve static content
+app.use( express.static('public') );
+
+app.get('/', function (req, res) {
+  res.render('home', {
+    name: 'Joaco',
+    title: 'Nodejs Course'
+  });
+});
+
+app.get('/generic', function (req, res) {
+  res.render('generic', {
+    name: 'Joaco',
+    title: 'Nodejs Course'
+  });
+});
+
+app.get('/elements', function (req, res) {
+  res.render('elements', {
+    name: 'Joaco',
+    title: 'Nodejs Course'
+  });
+  });
+
+app.get('/hello-world', function (req, res) {
+    res.send('Hello World')
+});
+
+app.get('*', function (req, res) {
+res.sendFile( __dirname + '/public/404.html');
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
